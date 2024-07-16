61fb809c84c28f5606bbb7b788dd92afb82d18b4
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2024-07-16 17:33:19 +0500

    feat: Add prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,15 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  // This removes the warning regarding React version being unset
   { ...pluginReactConfig, settings: { react: { version: "detect" } } },
   { ignores: ["dist/*"] },
-  { rules: {     "react/react-in-jsx-scope": "off" } }
+  { rules: { "react/react-in-jsx-scope": "off" } },
+  eslintConfigPrettier,
 ];
